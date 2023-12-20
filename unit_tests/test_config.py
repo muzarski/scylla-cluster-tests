1e9c7d32f06170ec057e8213ee1c5cdcc9014638
muzarski <mikolaj.uzarski@scylladb.com>
2023-12-20 11:47:07 +0100

    fix(loaders): retry cql-stress operation

--- unit_tests/test_config.py
+++ unit_tests/test_config.py
@@ -672,7 +672,7 @@ class ConfigurationTests(unittest.TestCase):  # pylint: disable=too-many-public-
                           'kcl': 'scylladb/hydra-loaders:kcl-jdk8-20210526-ShardSyncStrategyType-PERIODIC',
                           'harry': 'scylladb/hydra-loaders:cassandra-harry-jdk11-20220816',
                           'latte': 'scylladb/hydra-loaders:latte-rust1_73-20231025',
-                          'cql-stress-cassandra-stress': 'scylladb/hydra-loaders:cql-stress-cassandra-stress-20231130'})
+                          'cql-stress-cassandra-stress': 'scylladb/hydra-loaders:cql-stress-cassandra-stress-20231220'})
 
         self.assertEqual(conf.get('stress_image.gemini'), 'scylladb/hydra-loaders:gemini-v1.8.6')
         self.assertEqual(conf.get('stress_image.non-exist'), None)
